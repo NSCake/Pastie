d86fb8e622b91f82dc0892402c4a9154c4098bbd
Tanner Bennett <tannerbennett@me.com>
2025-06-04 22:02:51 -0500

    Meta tag parser maybe done

--- PBMetaTagParser.h
+++ PBMetaTagParser.h
@@ -11,7 +11,20 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PBMetaTagParser : NSObject <NSXMLParserDelegate>
 
++ (void)fetchMetaTagsForURL:(NSURL *)url completion:(void(^)(PBMetaTagParser *parser, NSError *error))callback;
+
 @property (nonatomic, readonly) NSDictionary<NSString *, NSString *> *ogTags;
+
+/// Such as "TikTok"
+@property (nonatomic, readonly) NSString *site;
+// Such as "TikTok · Some Creator"
+@property (nonatomic, readonly) NSString *title;
+/// The URL of the page
+@property (nonatomic, readonly) NSString *url;
+/// A thumbnail for the page; it may expire over time
+@property (nonatomic, readonly) NSString *image;
+/// A short description of the page
+@property (nonatomic, readonly) NSString *desc;
 
 @end
 
--- PBMetaTagParser.m
+++ PBMetaTagParser.m
@@ -5,11 +5,13 @@
 //  Created by Tanner Bennett on 3/10/24.
 //
 
+#import <UIKit/UIKit.h>
 #import "PBMetaTagParser.h"
 #import "NSArray+Map.h"
 
 @interface PBMetaTagParser ()
 
+/// The raw underlying tags, used to later populate `ogTags`
 @property (nonatomic) NSMutableArray<NSDictionary *> *metaTags;
 
 @end
@@ -21,6 +23,7 @@
     if (self) {
         _metaTags = [NSMutableArray new];
     }
+    
     return self;
 }
 
@@ -33,16 +36,77 @@
 }
 
 - (void)parserDidEndDocument:(NSXMLParser *)parser {
+    // Not sure why this would be needed twice...
     NSArray *ogTags = [self.metaTags pastie_filtered:^BOOL(NSDictionary *attrs, NSUInteger idx) {
         return [attrs[@"property"] hasPrefix:@"og:"];
     }];
     
+    // Pull the content out of the tag
     NSMutableDictionary *ogDict = [NSMutableDictionary new];
     for (NSDictionary *tag in ogTags) {
-        ogDict[tag[@"property"]] = tag[@"content"];
+        ogDict[tag[@"property"]] = [self unescapedTagContent:tag[@"content"]];
     }
     
     _ogTags = ogDict;
+    [self populateTagProperties];
+}
+
+- (NSString *)unescapedTagContent:(NSString *)content {
+    NSParameterAssert(content);
+    
+    NSData *data = [content dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *options = @{
+        NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+        NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)
+    };
+    NSAttributedString *attributedString = [[NSAttributedString alloc]
+        initWithData:data options:options documentAttributes:nil error:nil
+    ];
+
+    NSString *decodedString = attributedString.string;
+    return decodedString;
+}
+
+- (void)populateTagProperties {
+    _site = self.ogTags[@"og:site_name"];
+    _title = self.ogTags[@"og:title"];
+    _url = self.ogTags[@"og:url"];
+    _image = self.ogTags[@"og:image"];
+    _desc = self.ogTags[@"og:description"];
+}
+
++ (void)fetchMetaTagsForURL:(NSURL *)url completion:(void(^)(PBMetaTagParser *parser, NSError *error))callback {
+    NSParameterAssert(url); NSParameterAssert(callback);
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.allHTTPHeaderFields = @{
+        @"User-Agent": @"facebookexternalhit/1.1"
+    };
+
+    NSURLSessionDataTask *task = [NSURLSession.sharedSession
+        dataTaskWithRequest:request
+        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (error) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    callback(nil, error);
+                });
+            } else {
+                NSXMLParser *xmlParser = [[NSXMLParser alloc] initWithData:data];
+                PBMetaTagParser *parser = [PBMetaTagParser new];
+                xmlParser.delegate = parser;
+                
+                if ([xmlParser parse]) {
+                    [parser parserDidEndDocument:xmlParser];
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    callback(parser, nil);
+                });
+            }
+        }
+    ];
+    
+    [task resume];
 }
 
 @end
